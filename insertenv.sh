6df5d1b493289cf7fc4552eb0b6c7dc6b9a31f4d
terry.li <terry.li@dorry.io>
2017-01-19 17:46:46 +0800

    build 0.1.1

---
+++ insertenv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sed -i '$d' /var/www/html/LocalSettings.php
+
+echo "\
+\$wgServer = \"${SERVER_ADDR:-http://test-a:9000}\";\
+\$wgDBserver = \"${DB_SERVER_IP:-172.17.0.2}\";\
+\$wgDBuser = \"${DB_USER:-root}\";\
+\$wgDBpassword = \"${DB_PASSWORD:-abc123_}\";" >> /var/www/html/LocalSettings.php
