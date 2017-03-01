8963ae49d6c0beede4045992e9ff7201d69b1c4e
terry.li <terry.li@dorry.io>
2017-03-01 15:14:04 +0800

    modi Dockerfile&mode insertenv.sh

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM synctree/mediawiki
 COPY LocalSettings.php /var/www/html/
 #RUN chgrp www-data: /var/www/html/LocalSettings.php
 COPY insertenv.sh /
+COPY docker-entrypoint.sh /
 
-#ENTRYPOINT [""]
+ENTRYPOINT ["/docker-entrypoint.sh"]
 
 CMD ["/insertenv.sh"]
--- insertenv.sh
+++ insertenv.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 sed -i '$d' /var/www/html/LocalSettings.php
-sed -i '$d' /var/www/html/LocalSettings.php 
-sed -i '$d' /var/www/html/LocalSettings.php 
-sed -i '$d' /var/www/html/LocalSettings.php 
+sed -i '$d' /var/www/html/LocalSettings.php
+sed -i '$d' /var/www/html/LocalSettings.php
+sed -i '$d' /var/www/html/LocalSettings.php
 
 echo "\
 \$wgServer = \"${SERVER_ADDR:-http://test-a:9000}\";\n\
-\$wgDBserver = \"${DB_SERVER_IP:-172.17.0.2}\";\n\
-\$wgDBuser = \"${DB_USER:-root}\";\n\
-\$wgDBpassword = \"${DB_PASSWORD:-abc123_}\";" >> /var/www/html/LocalSettings.php
+\$wgDBserver = \"${MEDIAWIKI_DB_HOST:-172.17.0.2}\";\n\
+\$wgDBuser = \"root\";\n\
+\$wgDBpassword = \"${MEDIAWIKI_DB_PASSWORD:-abc123_}\";" >> /var/www/html/LocalSettings.php
 
 apache2-foreground
 #exec /entrypoint.sh
